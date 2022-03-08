c119f020268b940e1d3c17301b62030ef34ec8c6
vjauxysbrynxx <90746160+vjauxysbrynxx@users.noreply.github.com>
2022-03-08 14:23:15 +0800

    Add files via upload

---
+++ Lecture1/Assignments/assignment_lec1_c2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+    int main (void){
+
+        int x,y,z;
+        x=15;
+        y=87;
+        z=y-x;
+
+        printf("%d subtracted from %d is equal to %d.", x, y, z);
+
+    return 0;
+}

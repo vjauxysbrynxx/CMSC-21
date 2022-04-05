571158fead2fc0cce9af2a3b6213bca45af62497
vjauxysbrynxx <90746160+vjauxysbrynxx@users.noreply.github.com>
2022-04-05 14:53:31 +0800

    Add files via upload

---
+++ Lecture3/Assignments/as1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+    int main (void) {
+        int age;
+        bool teenager = false;
+
+        if (age <= 19 && age >= 13)
+            teenager = true;
+
+        return 0;
+    }
